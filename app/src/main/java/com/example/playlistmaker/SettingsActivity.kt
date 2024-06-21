ca17bc6c95fd2ae7d3a86bf065ae442059e65b2a
Aleksandr Antipin <alexandr.aantipin@yandex.ru>
2024-06-21 23:01:34 +0300

    Домашнее задание Спринт 9/29 → Тема 1/5: Стили и темы → Урок 3/3

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.playlistmaker
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
+
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,9 +14,39 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val buttonBack = findViewById<ImageView>(R.id.button_back)
+        val shareButton = findViewById<ImageView>(R.id.button_share)
+        val arrowButton = findViewById<ImageView>(R.id.button_arrow)
+        val supportButton = findViewById<ImageView>(R.id.button_support)
 
         buttonBack.setOnClickListener {
             onBackPressed()
+        }
+
+        supportButton.setOnClickListener {
+            val message = getString(R.string.string_email_text)
+            val tittle = getString(R.string.string_email_tittle)
+            val shareIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("digitex150@yandex.ru"))
+                putExtra(Intent.EXTRA_TEXT,message)
+                putExtra(Intent.EXTRA_SUBJECT,tittle)
+            }
+
+            startActivity(shareIntent)
+        }
+        arrowButton.setOnClickListener{
+            val url = Uri.parse(getString(R.string.url_arrow))
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(intent)
+        }
+
+        shareButton.setOnClickListener{
+            val message = getString(R.string.url_share)
+            val intent = Intent(Intent.ACTION_SEND).apply{
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, message)
+            }
+            startActivity(intent)
         }
 
     }
