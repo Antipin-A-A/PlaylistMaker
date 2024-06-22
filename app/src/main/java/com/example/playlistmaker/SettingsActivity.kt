2aff333c2e93d8a477bde7e81e939db4412aa5f2
Aleksandr Antipin <alexandr.aantipin@yandex.ru>
2024-06-22 10:10:58 +0300

    Спринт 9/29 → Тема 1/5: Стили и темы → Урок 3/3 поправил код

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -19,32 +19,28 @@ class SettingsActivity : AppCompatActivity() {
         val supportButton = findViewById<ImageView>(R.id.button_support)
 
         buttonBack.setOnClickListener {
-            onBackPressed()
+            onBackPressedDispatcher.onBackPressed()
         }
 
         supportButton.setOnClickListener {
-            val message = getString(R.string.string_email_text)
-            val tittle = getString(R.string.string_email_tittle)
             val shareIntent = Intent(Intent.ACTION_SENDTO).apply {
                 data = Uri.parse("mailto:")
                 putExtra(Intent.EXTRA_EMAIL, arrayOf("digitex150@yandex.ru"))
-                putExtra(Intent.EXTRA_TEXT,message)
-                putExtra(Intent.EXTRA_SUBJECT,tittle)
+                putExtra(Intent.EXTRA_TEXT,getString(R.string.string_email_text))
+                putExtra(Intent.EXTRA_SUBJECT,getString(R.string.string_email_tittle))
             }
 
             startActivity(shareIntent)
         }
         arrowButton.setOnClickListener{
-            val url = Uri.parse(getString(R.string.url_arrow))
-            val intent = Intent(Intent.ACTION_VIEW, url)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_arrow)))
             startActivity(intent)
         }
 
         shareButton.setOnClickListener{
-            val message = getString(R.string.url_share)
             val intent = Intent(Intent.ACTION_SEND).apply{
                 type = "text/plain"
-                putExtra(Intent.EXTRA_TEXT, message)
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.url_share))
             }
             startActivity(intent)
         }
