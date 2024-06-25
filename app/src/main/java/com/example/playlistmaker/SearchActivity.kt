33c652cd2c1d0fbc235fd4174fdf1a7f9177c564
Aleksandr.A <76969646+Antipin-A-A@users.noreply.github.com>
2024-06-26 01:42:35 +0300

    вернул состояние до спринта 9

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -14,9 +14,10 @@ import androidx.appcompat.app.AppCompatActivity
 class SearchActivity : AppCompatActivity() {
 
 
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-    }
 }
+    }
