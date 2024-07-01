a2767c81575af3f53db7b3f12e6fab594a0358dc
Aleksandr.A <76969646+Antipin-A-A@users.noreply.github.com>
2024-07-01 10:07:28 +0300

    Спринт 9 поправил

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -10,28 +10,23 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
-    private var getString : String = STRING
+    private var statusString : String = INPUTTEXT
 
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        if (savedInstanceState != null) {
-            getString = savedInstanceState.getString(KEY_STRING, STRING)
-        }
-
         val buttonBack = findViewById<TextView>(R.id.button_back_Search)
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val editText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.button_search_clean)
 
+
+
         clearButton.setOnClickListener {
-            inputEditText.setText("")
+            editText.setText("")
             val inputMethodManager =
                     getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
@@ -52,19 +47,29 @@ class SearchActivity : AppCompatActivity() {
                 } else {
                     clearButton.visibility = View.GONE
                 }
-
             }
 
             override fun afterTextChanged(p0 : Editable?) {
-                STRING = inputEditText.text.toString()
-
+                statusString = editText.text.toString()
             }
 
         }
-        inputEditText.addTextChangedListener(simpleTextWatcher)
+        editText.addTextChangedListener(simpleTextWatcher)
     }
+
+    override fun onSaveInstanceState(outState : Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_STRING, statusString)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        statusString = savedInstanceState.getString(KEY_STRING).toString()
+        findViewById<EditText>(R.id.inputEditText).setText(statusString)
+    }
+
     companion object {
-        const val KEY_STRING = "PRODUCT_AMOUNT"
-        var STRING = ""
+        const val KEY_STRING = "KEY_STRING"
+        const val INPUTTEXT = ""
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -4,25 +4,16 @@ package com.example.playlistmaker
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
-    private var status = CHECKED
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        if (savedInstanceState != null) {
-            status = savedInstanceState.getBoolean(
-                KEY_STATUS,
-                CHECKED
-            )
-        }
 
         val buttonBack = findViewById<TextView>(R.id.button_back)
         val shareButton = findViewById<ImageView>(R.id.button_share)
@@ -37,10 +28,8 @@ class SettingsActivity : AppCompatActivity() {
         switchButton.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                CHECKED = switchButton.isChecked
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                CHECKED = switchButton.isChecked
             }
         }
         supportButton.setOnClickListener {
@@ -48,11 +37,10 @@ class SettingsActivity : AppCompatActivity() {
             val tittle = getString(R.string.string_email_tittle)
             val shareIntent = Intent(Intent.ACTION_SENDTO).apply {
                 data = Uri.parse("mailto:")
-                putExtra(Intent.EXTRA_EMAIL, arrayOf("digitex150@yandex.ru"))
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.mail_adress_user)))
                 putExtra(Intent.EXTRA_TEXT, message)
                 putExtra(Intent.EXTRA_SUBJECT, tittle)
             }
-
             startActivity(shareIntent)
         }
         arrowButton.setOnClickListener {
@@ -69,12 +57,6 @@ class SettingsActivity : AppCompatActivity() {
             }
             startActivity(intent)
         }
-    }
-
-    companion object {
-        const val KEY_STATUS = "KEY_STATUS"
-        var CHECKED = false
-        const val KEY_SWITCH = "KEY_SWITCH"
     }
 
 }
