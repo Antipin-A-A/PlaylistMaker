52dc35f8d64cafb3f1059d57272272f94f10b11f
Aleksandr.A <76969646+Antipin-A-A@users.noreply.github.com>
2024-10-19 23:46:14 +0300

    Спринт 15 добавил rty/catch

--- app/src/main/java/com/example/playlistmaker/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/com/example/playlistmaker/data/network/RetrofitNetworkClient.kt
@@ -3,24 +3,30 @@ package com.example.playlistmaker.data.network
 import com.example.playlistmaker.data.NetworkClient
 import com.example.playlistmaker.data.dto.Response
 import com.example.playlistmaker.data.dto.TrackSearchRequest
+import java.io.IOException
 
-class RetrofitNetworkClient(private val retrofit: RetrofitManager, private val connectedManager: ConnectedManager) :
-    NetworkClient {
-
+class RetrofitNetworkClient(
+    private val retrofit: RetrofitManager, private val connectedManager: ConnectedManager
+) : NetworkClient {
     override fun doRequest(dto: Any): Response {
-        if (!connectedManager.isConnected()) {
-            return Response().apply { resultCode = -1 }
-        }
-
-        if (dto !is TrackSearchRequest) {
+        try {
+            if (!connectedManager.isConnected()) {
+                return Response().apply { resultCode = -1 }
+            }
+            if (dto is TrackSearchRequest) {
+                val response = retrofit.iTunesService.searchTrack(dto.expression).execute()
+                val dody = response.body() ?: Response()
+                return dody.apply { resultCode = response.code() }
+            } else {
+                return Response().apply { resultCode = 400 }
+            }
+        } catch (e: IOException) {
+            return Response().apply { resultCode = 400 }
+        } catch (e: RuntimeException) {
             return Response().apply { resultCode = 400 }
         }
-
-        val response = retrofit.iTunesService.searchTrack(dto.expression).execute()
-        val dody = response.body()
-        return dody?.apply { resultCode = response.code() } ?: Response().apply { resultCode = 400 }
     }
-
 }
+
 
 
