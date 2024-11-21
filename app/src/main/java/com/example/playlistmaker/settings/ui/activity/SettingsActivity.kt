a3f8b16c3007455ff57a6d638048fa1dc5ddd3ee
Aleksandr.A <76969646+Antipin-A-A@users.noreply.github.com>
2024-11-21 22:30:42 +0300

    Спринт 18 иправил background

--- app/src/main/java/com/example/playlistmaker/settings/ui/activity/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/settings/ui/activity/SettingsActivity.kt
@@ -2,7 +2,6 @@ package com.example.playlistmaker.settings.ui.activity
 
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playlistmaker.databinding.ActivitySettingsBinding
 import com.example.playlistmaker.settings.ui.viewmodel.SettingsViewModel
--- app/src/main/java/com/example/playlistmaker/settings/ui/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/example/playlistmaker/settings/ui/viewmodel/SettingsViewModel.kt
@@ -1,8 +1,5 @@
 package com.example.playlistmaker.settings.ui.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.playlistmaker.settings.domain.api.interact.ThemeInteractor
 import com.example.playlistmaker.sharing.domain.api.interact.SharingInteractor
